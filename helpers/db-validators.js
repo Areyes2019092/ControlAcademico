1da2ac4a3ce8eee61e0c12b776319b9ad13c9d76
Informatica <informatica@alumno.local>
2024-02-12 10:32:10 -0600

    Agruegue db-validators

---
+++ helpers/db-validators.js
@@ -0,0 +1,47 @@
+const Usuario = require("../models/usuario");
+const Role = require("../models/role");
+const Mascota = require("../models/mascota");
+
+const existenteEmailEstudiante = async (correo = "") => {
+  const existeEmailEstudiante = await Estudiante.findOne({ correo });
+  if (existeEmailEstudiante) {
+    throw new Error(`El email ${correo} ya fue registrado`);
+  }
+};
+
+const existenteEmailProfesor = async (correo = "") => {
+    const existeEmailProfesor = await Profesor.findOne({ correo });
+    if (existeEmailProfesor) {
+      throw new Error(`El email ${correo} ya fue registrado`);
+    }
+  };
+
+const existeEstudianteById = async (id = "") => {
+  const existeEstudiante = await Estudiante.findOne({ id });
+  if (existeEstudiante) {
+    throw new Error(`El estudiante con el ${id} no existe`);
+  }
+};
+
+const existeProfesorById = async (id = "") => {
+  const existeProfesor = await Profesor.findOne({ id });
+  if (existeProfesor) {
+    throw new Error(`El profesor con el id ${id} no existe`);
+  }
+};
+
+const esRolValido = async (role = "") => {
+  const existeRol = await Role.findOne({ role });
+
+  if (!existeRol) {
+    throw new Error(`El role ${role} no existe en base de datos.`);
+  }
+};
+
+module.exports = {
+    existenteEmailEstudiante,
+    existenteEmailProfesor,
+    existeEstudianteById,
+    existeProfesorById,
+    esRolValido,
+};
