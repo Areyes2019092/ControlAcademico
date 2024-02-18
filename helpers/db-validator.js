aa42c2704d64f8408a4b3c611231c95e927d4940
Angel <areyes-2019092@kinal.edu.gt>
2024-02-18 14:40:00 -0600

    Db Validators

--- helpers/db-validator.js
+++ helpers/db-validator.js
@@ -1,7 +1,9 @@
 const Materia = require("../models/materia.model");
 const Usuario = require("../models/user.model");
+
+
 const existeMaestroById = async (id = "") => {
-  const existeUsuario = await Usuario.findOne({ id });
+  const existeUsuario = await Usuario.findOne({ _id: id });
   if (!existeUsuario) {
     throw new Error(`El maestro con el id ${id} no existe`);
   } else if (existeUsuario.role !== "TEACHER_ROLE") {
@@ -9,31 +11,43 @@ const existeMaestroById = async (id = "") => {
   }
 };
 
-const existeUsuarioById = async (id = "") => {
-  const existeUsuario = await Usuario.findOne({ id });
-  if (existeUsuario) {
-    throw new Error(`El id ${id} no existe en MongoDB`);
-  };
-}
-  const existeMateriaById = async (id = "") => {
-    const existeMateria = await Materia.findOne({ id });
-    if (existeMateria) {
+const existeMateriaById = async (id = "") => {
+  
+    const existeMateria = await Materia.findOne({ _id: id });
+    if (!existeMateria) {
       throw new Error(`La materia con el id ${id} no existe`);
     }
-  };
+  }
 
-  const esRolValido = async (role = "") => {
-    const existeRol = await Role.findOne({ role });
 
-    if (!existeRol) {
-      throw new Error(`El role ${role} no existe en base de datos.`);
+  async function nombreExiste(nombre = "") {
+    const materia = await Materia.findOne({ nombre: nombre });
+    if(materia){
+      throw new Error(`La materia ya existe`);
     }
-  };
+  }
+
+const existeUsuarioById = async (id = "") => {
+
+    const existeUsuario = await Usuario.findOne({ _id: id });
+    if (!existeUsuario) {
+      throw new Error(`El usuario con el id ${id} no existe`);
+    }
+  }
+
+
+async function correoExiste(correo = "") {
+  const usuario = await Usuario.findOne({ correo: correo });
+  if(usuario){
+    throw new Error(`El usuario ya existe`);
+  }
+}
 
 
 module.exports = {
   existeMaestroById,
   existeMateriaById,
+  correoExiste,
   existeUsuarioById,
-  esRolValido,
+  nombreExiste
 };
