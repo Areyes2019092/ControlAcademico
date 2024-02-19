f4749d331369fdefc7eea3bbe80a7ecebea4fa6e
Angel <areyes-2019092@kinal.edu.gt>
2024-02-18 18:15:18 -0600

    Cree un alumnoCurso

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,99 +1,116 @@
-const { response, json } = require("express");
+const { response, json } = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/user.model');
+const { generarJWT } = require("../helpers/jwt");
 
-const bcrypt = require("bcrypt");
+const usuariosGet = async (req, res = response) => {
+    const { limite, desde } = req.query;
+    const query = { estado: true };
 
-const Usuario = require("../models/user.model");
-const { check } = require("express-validator");
-const { existeMaestroById } = require("../helpers/db-validator");
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ]);
 
-const getUser = async (req, res = response) => {
-  
-  const query = { estado: true };
-
-  const [total, usuarios] = await Promise.all([
-    Usuario.countDocuments(query),
-    Usuario.find(query),
-  ]);
-
-  res.status(200).json({
-    total,
-    usuarios,
-  });
-};
-
-const getUserById = async (req, res) => {
-  const { id } = req.params;
-  const usuario = await Usuario.findOne({ _id: id });
-
-  res.status(200).json({
-    usuario,
-  });
-};
-
-const usuariosPost = async (req, res) => {
-  const { nombre, correo, password, role } = req.body;
-  const usuario = new Usuario({ nombre, correo, password, role });
-
-  await usuario.save();
-  res.status(200).json({
-    usuario,
-  });
-};
-
-const usuariosPostSTUDENT = async (req, res) => {
-  const { nombre, correo, password } = req.body;
-  const usuario = new Usuario({ nombre, correo, password });
-
-  await usuario.save();
-  res.status(200).json({
-    usuario,
-  });
-};
-
-
-const loginUsers = async (req, res) => {
-const { correo, password } = req.body;
-const usuario = await Usuario.findOne({correo: correo , password: password });
-if(!usuario){
-  return res.status(400).json({ msg: "Usuario incorrecto" });
+    res.status(200).json({
+        total,
+        usuarios
+    });
 }
-res.status(200).json({
-  msg: "Bienvenido"
-})
 
+const getUsuarioByid = async (req, res) => {
+    const { id } = req.params;
+    const usuario = await Usuario.findOne({ _id: id });
+    res.status(200).json({
+        usuario
+    });
 }
+
 
 const usuariosPut = async (req, res) => {
-  const { id } = req.params;
-  const { maestroId, correo, password, role, ...resto } = req.body;
-  await Usuario.findByIdAndUpdate(id, resto);
+    const { id } = req.params;
+    const { _id, password, google, correo, ...resto } = req.body;
 
-  const usuario = await Usuario.findOne({ _id: id });
+    const usuario = await Usuario.findByIdAndUpdate(id, resto);
 
-  res.status(200).json({
-    msg: "Usuario actualizado exitosamente",
-    usuario,
-  });
-};
+    res.status(200).json({
+        msg: 'Actualizado'
+    })
+}
 
 const usuariosDelete = async (req, res) => {
-  const { id } = req.params;
-  await Usuario.findByIdAndUpdate(id, { estado: false });
+    const { id } = req.params;
+    const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
 
-  const usuario = await Usuario.findOne({ _id: id });
+    res.status(200).json({
+        msg: 'Eliminado'
+    });
+}
 
-  res.status(200).json({
-    msg: "Usuario eliminado exitosamente",
-    usuario,
-  });
-};
+const usuariosPost = async (req, res) => {
+    const { nombre, correo, password, role } = req.body;
+    const usuario = new Usuario({ nombre, correo, password, role });
+
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    await usuario.save();
+    res.status(200).json({
+        usuario
+    });
+}
+
+const usuariosLogin = async (req, res) => {
+    const { correo, password } = req.body;
+
+    try{
+        const usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+        return res.status(400).json({
+            msg: 'No encontrado'
+        });
+    }
+
+    if(!usuario.estado){
+        return res.status(400).json({
+            msg: 'Usuario ya no existe'
+        })
+    }
+
+    const passwordValido = bcryptjs.compareSync(password, usuario.password);
+
+    if (!passwordValido) {
+        return res.status(400).json({
+            msg: 'La contraseña no es correcta'
+        });
+    }
+
+    const token = await generarJWT(usuario.id)
+
+    res.status(200).json({
+        msg_1: 'Se inicio sesion',
+        msg_2: 'Bienvenido ',
+        msg_3: 'token'+ token,
+    });
+
+    }catch(e){
+        console.log(e);
+        res.status(500).json({
+            msg: 'No se pudo iniciar sesion'
+        })
+    }
+
+}
+
 
 module.exports = {
-  getUser,
-  getUserById,
-  usuariosPost,
-  usuariosPut,
-  usuariosDelete,
-  usuariosPostSTUDENT,
-  loginUsers
-};
+    usuariosDelete,
+    usuariosPost,
+    usuariosGet,
+    getUsuarioByid,
+    usuariosPut,
+    usuariosLogin
+}
